38bf5efc980553ec4a70a268591d9c1b8ee4ad59
rpaskalev <rpaskalev@lomixtech.com>
2021-11-16 19:50:30 -0500

    Add ALB ASG TG to code

--- ec2.tf
+++ ec2.tf
@@ -44,3 +44,8 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.my_subnet_1.id
   route_table_id = aws_route_table.training.id
 }
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.my_subnet_2.id
+  route_table_id = aws_route_table.training.id
+}
--- key_pair.tf
+++ key_pair.tf
@@ -1,20 +1,20 @@
-resource "aws_key_pair" "ec2_key_pair" {
-  key_name   = "ziyote-class-key"
-  public_key = tls_private_key.example.public_key_openssh
-}
+# resource "aws_key_pair" "ec2_key_pair" {
+#   key_name   = "ziyote-class-key"
+#   public_key = tls_private_key.example.public_key_openssh
+# }
 
-resource "tls_private_key" "example" {
-  algorithm   = "RSA"
-  ecdsa_curve = "P384"
-}
+# resource "tls_private_key" "example" {
+#   algorithm   = "RSA"
+#   ecdsa_curve = "P384"
+# }
 
-#show exmple to send key to file:
+# #show exmple to send key to file:
 
-resource "local_file" "private_key" {
-    content  = tls_private_key.example.private_key_pem
-    filename = "${var.path}ziyote-class-key.pem"
-}
+# resource "local_file" "private_key" {
+#     content  = tls_private_key.example.private_key_pem
+#     filename = "${var.path}ziyote-class-key.pem"
+# }
 
-variable "path" {
-  default = "../../../Keys/"
-}
+# variable "path" {
+#   default = "../../../Keys/"
+# }
