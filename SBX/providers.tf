7551cf5f0cb66fb9a895854c2a32981a6e77d89c
rpaskalev <rpaskalev@lomixtech.com>
2021-12-04 22:57:17 -0500

    edit10

--- SBX/providers.tf
+++ SBX/providers.tf
@@ -1,3 +1,12 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "rady-bucket-1-0-0"
+    key    = "dags/myfile"
+    region = "us-east-1"
+  }
+}
